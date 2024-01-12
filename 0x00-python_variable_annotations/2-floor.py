0f94052625fb2a1a8e999da2c7af471b82439e2a
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-12 01:42:13 +0100

    vriable_annotations

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""function that calculates the floor of a number."""
+"""import model"""
 import math
 
 
 def floor(n: float) -> int:
+    """function that calculates the floor of a number."""
     return math.floor(n)
