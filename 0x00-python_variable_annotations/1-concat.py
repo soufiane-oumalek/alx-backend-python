175bb26a663f13f54317ca372fa46b66f5d5bf6e
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-12 01:43:39 +0100

    vriable_annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""function that concatenates strings."""
 
 
 def concat(str1: str, str2: str) -> str:
+    """function that concatenates strings."""
     return str1 + str2
