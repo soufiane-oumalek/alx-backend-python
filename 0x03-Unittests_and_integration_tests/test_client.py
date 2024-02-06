a38e4195722e5e22300384032be556b8e61629c2
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-06 01:14:14 +0100

    unitests_and_intergration

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,21 +9,17 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """
-    Class for Testing Github Org Client
-    """
+    """ Class testing githubOrgClient"""
     @parameterized.expand([
         ('google'),
         ('abc')
     ])
     @patch('client.get_json')
     def test_org(self, org_name: str, mock_get: Mock) -> None:
-        """
-        Test that GithubOrgClient.org returns the correct value
-        """
+        """Test GithubOrgClient"""
         test_class = GithubOrgClient(org_name)
         test_class.org()
-        url = (f'https://api.github.com/orgs/{input}')
+        url = (f'https://api.github.com/orgs/{org_name}')
         mock_get.assert_called_once_with(url)
 
     def test_public_repos_url(self):
