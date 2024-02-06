d97952e6c112e91cfccd8a8aabf85e055fa66d59
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-06 01:28:05 +0100

    unitests_and_intergration

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
-"""
-testing client
-"""
-import unittest
-from unittest.mock import patch, PropertyMock
+""" doc doc doc """
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
+import unittest
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """
-    Class testing githubOrgClient
-    """
-    @parameterized.expand([
-        ("google"),
-        ("abc"),
-    ])
-    @patch('client.get_json')
+    """doc doc doc"""
+
+    @parameterized.expand(
+        [
+            ("google"),
+            ("abc"),
+        ]
+    )
+    @patch("client.get_json", return_value={"payload": True})
     def test_org(self, org_name: str, mock_get: Mock) -> None:
-        """Test GithubOrgClient"""
-        test_class = GithubOrgClient(org_name)
-        test_class.org()
+        """doc doc doc"""
+        github_org_client = GithubOrgClient(org_name)
+        self.assertEqual(github_org_client.org, {"payload": True})
         url = f"https://api.github.com/orgs/{org_name}"
         mock_get.assert_called_once_with(url)
 
